f095961d6b1fe12db99d9f6f822c7ffb69552a60
kaitdoch Docherty <kaitlyn.docherty@kellogg.northwestern.edu>
2025-06-04 04:30:25 +0000

    sign up and login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "username" => params["username"] })
+    if @user != nil
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "You are now logged-in."
+        redirect_to "/"
+      else
+        flash["notice"] = "Sorry, invalid username or password."
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Sorry, invalid username or password."
+      redirect_to "/login"
+    end
   end
 
   def destroy
